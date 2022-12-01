54cdc4cf456225b90aaa8adef890fa7dc521efe3
Felipe Thome <119542554+felipetn1989@users.noreply.github.com>
2022-12-01 16:29:03 -0300

    alg034 start

--- Lista de exerícios algoritmos/alg034/script.js
+++ Lista de exerícios algoritmos/alg034/script.js
@@ -1,6 +1,34 @@
 var fkm = document.querySelector('input#fkm')
+var fdias = document.querySelector('input#fdias')
+var resultado = document.querySelector('div#resultado')
 
-function calcular () {
-    var km = Number(fkm.value)
-    alert (km)
+function calcular() {
+    var select = document.myForm.mySelect.selectedIndex
+    var km = Number (fkm.value)
+    var dias = Number (fdias.value)
+
+    if (fkm.value.length == 0 || select == 0 || fdias.value.length ==0) {
+        alert('Preencha todos os dados e tente novamente')
+    } else if (km < 0) {
+        alert('Valor de Kms incorreto')
+    }
+
+    if (select == 1) {
+        var diaria = 90
+        if (km <= 100) {
+            var kmcost = 0.20
+        } else {
+            var kmcost = 0.10
+        }
+    } else {
+        var diaria = 150
+        if (km <= 200) {
+            var kmcost = 0.30
+        } else {
+            var kmcost = 0.25
+        }
+    }
+   
+    var rentalprice = (dias*diaria) + (km*kmcost)
+    alert(rentalprice)
 }
