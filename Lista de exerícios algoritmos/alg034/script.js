3ee336276216dc925c5c9decf1e8829744624fbc
Felipe Thome <119542554+felipetn1989@users.noreply.github.com>
2022-12-02 06:55:10 -0300

    further development alg034

--- Lista de exerícios algoritmos/alg034/script.js
+++ Lista de exerícios algoritmos/alg034/script.js
@@ -14,31 +14,32 @@ function calcular() {
         alert('Preencha todos os dados e tente novamente')
     } else if (km < 0) {
         alert('Valor de Kms incorreto')
-    }
-
-    if (select == 1) {
-        var diaria = 90
-        if (km <= 100) {
-            var kmcost = 0.20
-        } else {
-            var kmcost = 0.10
-        }
-        foto.setAttribute ('src','imagens/carro-pop-pq.png')
-        
     } else {
-        var diaria = 150
-        if (km <= 200) {
-            var kmcost = 0.30
+        if (select == 1) {
+            var diaria = 90
+            if (km <= 100) {
+                var kmcost = 0.20
+            } else {
+                var kmcost = 0.10
+            }
+            foto.setAttribute ('src','imagens/carro-pop-pq.png')
+            
         } else {
-            var kmcost = 0.25
+            var diaria = 150
+            if (km <= 200) {
+                var kmcost = 0.30
+            } else {
+                var kmcost = 0.25
+            }
+            foto.setAttribute ('src','./imagens/carro-luxo-pq.png')
         }
-        foto.setAttribute ('src','./imagens/carro-luxo-pq.png')
+       
+        var rentalprice = (dias*diaria) + (km*kmcost)
+    
+        var fprice = document.createElement('p')
+        fprice.innerHTML = (`O valor total da locação é de R$${rentalprice.toFixed(2)}`)
+        resultado.appendChild (fprice)
+        resultado.appendChild (foto)
     }
-   
-    var rentalprice = (dias*diaria) + (km*kmcost)
 
-    var fprice = document.createElement('p')
-    fprice.innerHTML = (`O valor total da locação é de R$${rentalprice.toFixed(2)}`)
-    resultado.appendChild (fprice)
-    resultado.appendChild (foto)
 }
