7572499110c82204e099b678f4c40b0943b45f2c
Felipe Thome <119542554+felipetn1989@users.noreply.github.com>
2022-12-03 18:57:01 -0300

    alg054 possible solution

--- Lista de exerícios algoritmos/alg054/script.js
+++ Lista de exerícios algoritmos/alg054/script.js
@@ -1,28 +1,42 @@
 var falt = document.querySelector('input#falt')
 var fpeso = document.querySelector('input#fpeso')
 var but = document.querySelector('input#button')
+var resultado = document.querySelector('div#resultado')
+
 var altvec = []
-var altpeso = []
+var pesovec = []
+
+let c = 1
 
 function add () {
-
-    for (var c = 1; c <= 7; c++) {
-        if (falt.value.length != 0 && fpeso.value.length != 0) {
-            var alt = Number(falt.value)
-            var peso = Number(fpeso.value)
+    if (c <= 7) {
+        var alt = Number(falt.value)
+        var peso = Number(fpeso.value)
         
-            altvec.push(alt)
-            altpeso.push(peso)
-    
-            but.setAttribute('value',`Adicionar pessoa ${c+1}`)
+        altvec.push(alt)
+        pesovec.push(peso)
         
-            falt.value = ('')
-            fpeso.value = ('')
-            
-            falt.focus()
-        } 
+        falt.value = ('')
+        fpeso.value = ('')
+        falt.focus()
+        c ++
+    } else {
+        alert('Máximo de dados atingidos, clicar em Finalizar')
     }
 }
+
+function finalizar () {
+    resultado.innerHTML = (`${altvec}`)
+}
+
+
+
+
+
+ 
+
+
+
 
 
     
