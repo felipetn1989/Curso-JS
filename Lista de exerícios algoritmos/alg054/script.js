b0947e5078cccc5fe27d1572c10aa6b4910d395f
Felipe Thome <119542554+felipetn1989@users.noreply.github.com>
2022-12-03 19:47:34 -0300

    alg054 finalized

--- Lista de exerícios algoritmos/alg054/script.js
+++ Lista de exerícios algoritmos/alg054/script.js
@@ -1,32 +1,44 @@
-var falt = document.querySelector('input#falt')
-var fpeso = document.querySelector('input#fpeso')
+var fheight = document.querySelector('input#falt')
+var fweight = document.querySelector('input#fpeso')
 var but = document.querySelector('input#button')
-var resultado = document.querySelector('div#resultado')
+var pbut = document.querySelector('p.button')
 
-var altvec = []
-var pesovec = []
+var result = document.querySelector('div#result')
+
+var hvec = []
+var wvec = []
 
 let c = 1
+let hsum = 0
 
 function add () {
-    if (c <= 7) {
-        var alt = Number(falt.value)
-        var peso = Number(fpeso.value)
+    if (fheight.value.length == 0 || fweight.value.length == 0) {
+        alert('Complete the information and try again')
+    } else if (c <= 7) {
+        var height = Number(fheight.value)
+        var weight = Number(fweight.value)
         
-        altvec.push(alt)
-        pesovec.push(peso)
+        hvec.push(height.toFixed(2))
+        wvec.push(weight)
         
-        falt.value = ('')
-        fpeso.value = ('')
-        falt.focus()
+        fheight.value = ('')
+        fweight.value = ('')
+        fheight.focus()
+
+        hsum += height
         c ++
-    } else {
-        alert('Máximo de dados atingidos, clicar em Finalizar')
+
+        if (c == 8) {
+            alert('Maximum data size reached. Click on finalize')
+            pbut.removeChild(but)
+        }
     }
 }
 
-function finalizar () {
-    resultado.innerHTML = (`${altvec}`)
+function finalize () {
+    result.innerHTML = (`
+        Average height of the group: ${(hsum)/(hvec.length)}
+    `)
 }
 
 
