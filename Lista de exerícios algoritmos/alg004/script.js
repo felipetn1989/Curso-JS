9e553f67581e6924634dbdd04c9ab351753cb391
Felipe Thome <119542554+felipetn1989@users.noreply.github.com>
2022-12-12 19:31:21 -0300

    update alg004

--- Lista de exerícios algoritmos/alg004/script.js
+++ Lista de exerícios algoritmos/alg004/script.js
@@ -1,17 +1,10 @@
 var n1txt = window.document.querySelector('input#n1')
 var n2txt = window.document.querySelector('input#n2')
-var resultado = document.querySelector('div#resultado')
+var isoma = document.querySelector('output#isoma')
 
 function somar() {
-    if (n1txt.value.length == 0 || n2txt.value.length == 0) {
-        alert('Preencha os dados e tente novamente')
-    } else {
         var n1 = Number(n1txt.value)
         var n2 = Number(n2txt.value)
         let soma = n1 + n2
-        resultado.innerHTML = (`O valor da soma entre ${n1} e ${n2} é igual a ${soma}`)
-        n1txt.value = ('')
-        n2txt.value = ('')
-        n1txt.focus()
-    }
+        isoma.innerHTML = soma
 }
